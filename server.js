ecdb6349911287da69b63ef33e415d05c2fcaa1c
Elizabeth Paul <elizabethpaul@elizabeths-mbp.home>
2020-03-17 18:40:06 +0000

    commit

--- server.js
+++ server.js
@@ -2,34 +2,22 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('/helpers/create_router.js');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
 
-var express = require('express')
-var cors = require('cors')
-var app = express()
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(cors())
-
-app.get('/products/:id', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('garden');
+  const plantsCollection = db.collection('plants');
+  const plantsRouter = createRouter(plantsCollection);
+  app.use('/api/garden', plantsRouter);
 })
+.catch(console.err);
 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
+app.listen(3000, function() {
+  console.log(`Listening on port ${
+    this.address().port}`);
 })
-
-const db = process.env.MONGODB_URL;
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(db, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
-    console.log("MongoDB is Connected...");
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
